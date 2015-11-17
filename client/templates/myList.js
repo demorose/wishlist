36fc1b33a6210d917f026590f0ba4d352851f217
Emmanuel Leveque <eleveque@hi-media.com>
2015-11-17 15:28:09 +0100

    Update style

--- client/templates/myList.js
+++ client/templates/myList.js
@@ -9,6 +9,18 @@ Template.myList.helpers({
 Template.myList.events({
     'submit form': function(e) {
         e.preventDefault();
-        
+    },
+    'click #addList': function(e) {
+        e.preventDefault();
+        Meteor.call('createList');
+    },
+});
+
+Template.ownList.events({
+    'click .delete': function(e) {
+        e.preventDefault();
+        if (confirm("Delete this list?")) {
+            Meteor.call('deleteList', this._id);
+        }
     },
 });
