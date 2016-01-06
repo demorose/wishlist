69775a235e91937766ac77151a336d89015f7671
Emmanuel LEVEQUE <emmanuel@leveque.it>
2016-01-06 14:12:07 +0100

    Beginning share feature

--- client/templates/myList.js
+++ client/templates/myList.js
@@ -41,35 +41,41 @@ Template.ownList.events({
     'click .share': function(e) {
         e.preventDefault;
         var list = this;
-        bootbox.dialog({
-            title: "Share this list with:" + list.name,
-            message: '<div class="row">  ' +
+        var message = '<div class="row">  ' +
                 '<div class="col-md-12"> ' +
                 '<form class="form-horizontal"> ' +
-                '<div class="form-group"> ' +
-                '<label class="col-md-4 control-label" for="name">Name</label> ' +
-                '<div class="col-md-4"> ' +
-                '<input id="name" name="name" type="text" placeholder="Your name" class="form-control input-md"> ' +
-                '<span class="help-block">Here goes your name</span> </div> ' +
-                '</div> ' +
-                '<div class="form-group"> ' +
-                '<label class="col-md-4 control-label" for="awesomeness">How awesome is this?</label> ' +
-                '<div class="col-md-4"> <div class="radio"> <label for="awesomeness-0"> ' +
-                '<input type="radio" name="awesomeness" id="awesomeness-0" value="Really awesome" checked="checked"> ' +
-                'Really awesome </label> ' +
-                '</div><div class="radio"> <label for="awesomeness-1"> ' +
-                '<input type="radio" name="awesomeness" id="awesomeness-1" value="Super awesome"> Super awesome </label> ' +
-                '</div> ' +
+                '<div class="form-group"> ';
+
+        var users =  Meteor.users.find({_id : {$ne : Meteor.userId()}})
+        users.forEach(function(user) {
+            message += '<div class="checkbox-inline"> \
+                <label> \
+                    <input name="users[]" type="checkbox" id="" value="' + user._id + '" aria-label=""> \
+                    ' + user.username + ' \
+                </label> \
+            </div>'
+        });
+
+        message += '</div> ' +
                 '</div> </div>' +
-                '</form> </div>  </div>',
+                '</form> </div>  </div>';
+
+        bootbox.dialog({
+            title: list.name,
+            message: message,
             buttons: {
                 success: {
-                    label: "Save",
+                    label: TAPi18n.__("share"),
                     className: "btn-success",
                     callback: function () {
-                        var name = $('#name').val();
-                        var answer = $("input[name='awesomeness']:checked").val()
-                        Example.show("Hello " + name + ". You've chosen <b>" + answer + "</b>");
+                        var users = [];
+                        var checkboxes = $('input[name="users[]"');
+                        checkboxes.each(function() {
+                            if (this.checked) {
+                                users.push(this.value);
+                            }
+                        });
+                        console.log(users);
                     }
                 }
             }
