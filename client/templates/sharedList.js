0aec6da2d0888abb8264fd333c59cf7d7a78f73a
Emmanuel LEVEQUE <emmanuel@leveque.it>
2016-01-20 18:25:11 +0100

    You can't uncheck something you didn't check

--- client/templates/sharedList.js
+++ client/templates/sharedList.js
@@ -19,10 +19,23 @@ Template.sharedList.helpers({
     }
 })
 
+Template.sharedItem.helpers({
+    checkerUser : function() {
+        var item = this;
+        return Meteor.users.findOne(item.checker);
+    },
+    canUncheck : function() {
+        return (this.checker == Meteor.userId());
+    }
+})
+
 Template.sharedItem.events({
     'click .shared_item input[type="checkbox"]': function(event) {
+        event.preventDefault();
         var item = this;
         var checked = event.target.checked;
-        Meteor.call('checkItem', item._id, checked );
+        if(item.checker == Meteor.userId() || checked ) {
+            Meteor.call('checkItem', item._id, checked );
+        }
     }
 })
